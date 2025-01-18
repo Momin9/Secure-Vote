62c7a9ad60761182b4dec67bf01fee72aa0655bd
Momin Ali <mominalikhoker589@gmail.com>
2025-01-19 04:11:33 +0500

    first commit

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
+import os
+
 from app import app
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
